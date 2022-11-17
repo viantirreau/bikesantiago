b9571949e09eeda540e5b1e26759c2c42ee35605
Victor Tirreau <viantirreau@gmail.com>
2022-11-17 00:22:49 -0300

    feat(stations): add stations to map

--- visualization/js/main.js
+++ visualization/js/main.js
@@ -2,6 +2,7 @@ const WIDTH = 1000;
 const HEIGHT = 700;
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidmlhbnRpcnJlYXUiLCJhIjoiY2xhazJpOHdyMDF2MTNwbnMxbzkzMmpsayJ9.7tkQiRhR40t8P7_vXCRXfw'
 const MAP_STYLE = 'viantirreau/clajy669c002414lgwdtdkz22'
+let STATION_DATA = null;
 
 // Adapted from https://observablehq.com/@d3/zoomable-map-tiles
 const svg = d3
@@ -10,11 +11,45 @@ const svg = d3
     .attr("width", WIDTH)
     .attr("height", HEIGHT);
 
+let image = svg.append("g")
+    .attr("pointer-events", "none")
+    .selectAll("image");
+
 const tile = d3.tile()
     .extent([[0, 0], [WIDTH, HEIGHT]])
     .tileSize(512)
     .clampX(false);
 const mapboxUrl = (x, y, z) => `https://api.mapbox.com/styles/v1/${MAP_STYLE}/tiles/${z}/${x}/${y}@2x?access_token=${MAPBOX_ACCESS_TOKEN}`;
+
+const projection = d3.geoMercator()
+    .scale(1 / (2 * Math.PI))
+    .translate([0, 0]);
+
+
+const renderStations = (data, transform) => {
+    if (!data) return;
+    svg
+        .selectAll("circle")
+        .attr("cx", (d) => projection([d.lon, d.lat])[0])
+        .attr("cy", (d) => projection([d.lon, d.lat])[1])
+        .attr("r", 3)
+        .attr("fill", "red");
+}
+
+
+d3.csv("processed-data/meta.csv").then((data) => {
+    STATION_DATA = data;
+    svg
+        .selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => projection([d.lon, d.lat])[0])
+        .attr("cy", (d) => projection([d.lon, d.lat])[1])
+        .attr("r", 3)
+        .attr("fill", "red");
+});
+
 
 const zoomed = (transform) => {
     const tiles = tile(transform);
@@ -25,43 +60,28 @@ const zoomed = (transform) => {
         .attr("y", ([, y]) => (y + tiles.translate[1]) * tiles.scale)
         .attr("width", tiles.scale)
         .attr("height", tiles.scale);
+
+    projection
+        .scale(transform.k / (2 * Math.PI))
+        .translate([transform.x, transform.y]);
+
+    renderStations(STATION_DATA, transform);
+
 }
 
 
 const zoom = d3.zoom()
     .scaleExtent([1 << 19, 1 << 22])
     .extent([[0, 0], [WIDTH, HEIGHT]])
-    .on("zoom", ({ transform }) => { console.log(transform); zoomed(transform) });
+    .on("zoom", ({ transform }) => { zoomed(transform) });
 
-let image = svg.append("g")
-    .attr("pointer-events", "none")
-    .selectAll("image");
 
 svg
     .call(zoom)
+    // Centered on Santiago .translate(103380, -51420)
     .call(zoom.transform, d3.zoomIdentity
-        .translate(103380, -51420) // Centered on Santiago
-        .scale(1 << 19));
+        .translate(WIDTH / 2, HEIGHT / 2)
+        .scale(-(1 << 19))
+        .translate(...projection([-70.6693, -33.4489]))
+        .scale(-1));
 
-// d3.json("processed-data/comunas.json").then((data) => {
-//     projection = d3.geoIdentity().reflectY(true).fitSize([WIDTH, HEIGHT], data);
-//     svg
-//         .selectAll("path")
-//         .data(data.features)
-//         .enter()
-//         .append("path")
-//         .attr("d", d3.geoPath().projection(projection))
-//         .attr("fill", "lightblue")
-//         .attr("stroke", "#333");
-//     return projection;
-// }).then(projection => d3.csv("processed-data/meta.csv").then((data) => {
-//     svg
-//         .selectAll("circle")
-//         .data(data)
-//         .enter()
-//         .append("circle")
-//         .attr("cx", (d) => projection([d.lon, d.lat])[0])
-//         .attr("cy", (d) => projection([d.lon, d.lat])[1])
-//         .attr("r", 2)
-//         .attr("fill", "red");
-// }));
