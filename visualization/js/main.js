8c3355bff64cc310b88822713d894d418d622c70
Victor Tirreau <viantirreau@gmail.com>
2022-11-18 07:21:48 -0300

    feat(hexbin): add first hexbin prototype

--- visualization/js/main.js
+++ visualization/js/main.js
@@ -3,8 +3,11 @@ const HEIGHT = 700;
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidmlhbnRpcnJlYXUiLCJhIjoiY2xhazJpOHdyMDF2MTNwbnMxbzkzMmpsayJ9.7tkQiRhR40t8P7_vXCRXfw'
 const MAP_STYLE = 'viantirreau/clajy669c002414lgwdtdkz22'
 let STATION_DATA = null;
+let STATION_BINS = null;
 
 // Adapted from https://observablehq.com/@d3/zoomable-map-tiles
+// https://observablehq.com/@d3/zoomable-raster-vector
+
 const svg = d3
     .select("body")
     .append("svg")
@@ -25,9 +28,13 @@ const projection = d3.geoMercator()
     .scale(1 / (2 * Math.PI))
     .translate([0, 0]);
 
+const hexbin = d3.hexbin()
+    .x(d => projection([d.lon, d.lat])[0])
+    .y(d => projection([d.lon, d.lat])[1])
+    .extent([[0, 0], [WIDTH, HEIGHT]])
+    .radius(10);
 
-const renderStations = (data, transform) => {
-    if (!data) return;
+const renderStations = () => {
     svg
         .selectAll("circle")
         .attr("cx", (d) => projection([d.lon, d.lat])[0])
@@ -36,6 +43,15 @@ const renderStations = (data, transform) => {
         .attr("fill", "red");
 }
 
+const renderHexbin = (data, transform) => {
+    if (!STATION_DATA) return;
+    console.log(tile(transform));
+    data = hexbin(STATION_DATA);
+    svg.selectAll(".hexagon")
+        .data(data)
+        .attr("transform", d => `translate(${d.x},${d.y})`)
+        .attr("d", hexbin.hexagon());
+}
 
 d3.csv("processed-data/meta.csv").then((data) => {
     STATION_DATA = data;
@@ -48,6 +64,21 @@ d3.csv("processed-data/meta.csv").then((data) => {
         .attr("cy", (d) => projection([d.lon, d.lat])[1])
         .attr("r", 3)
         .attr("fill", "red");
+    STATION_BINS = hexbin(data);
+
+    svg
+        .append("g")
+        .selectAll("path")
+        .data(STATION_BINS)
+        .join("path")
+        .attr("transform", d => `translate(${d.x},${d.y})`)
+        .attr("class", "hexagon")
+        .attr("d", hexbin.hexagon(10))
+        .attr("fill", "#ccc")
+        .attr("fill-opacity", 0.5)
+        .attr("stroke", "red")
+        .append("title");
+
 });
 
 
@@ -64,9 +95,9 @@ const zoomed = (transform) => {
     projection
         .scale(transform.k / (2 * Math.PI))
         .translate([transform.x, transform.y]);
-
+    hexbin.radius(10 * transform.k / (1 << 20));
     renderStations(STATION_DATA, transform);
-
+    renderHexbin(STATION_BINS, transform);
 }
 
 
@@ -81,7 +112,7 @@ svg
     // Centered on Santiago .translate(103380, -51420)
     .call(zoom.transform, d3.zoomIdentity
         .translate(WIDTH / 2, HEIGHT / 2)
-        .scale(-(1 << 19))
-        .translate(...projection([-70.6693, -33.4489]))
+        .scale(-(1 << 20))
+        .translate(...projection([-70.6, -33.43]))
         .scale(-1));
 
